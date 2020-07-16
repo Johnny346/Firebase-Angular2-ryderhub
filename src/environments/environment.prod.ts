68a480fc1232401614b82898d1e32d4bfd2f5b6f
John <johnnymul346@gmail.com>
2020-07-16 08:02:48 +0100

    update environmeent file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
+
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBqxiKOaqz11VwPR0P5u2bpAK2oB5baA58',
+    authDomain: 'ryderhubproject.firebaseapp.com',
+    databaseURL: 'https://ryderhubproject.firebaseio.com',
+    projectId: 'ryderhubproject',
+    storageBucket: '',
+    messagingSenderId: '1044561798783'
+  }
 };
